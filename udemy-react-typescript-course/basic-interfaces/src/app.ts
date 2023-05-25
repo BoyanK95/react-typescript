868493d68adf08160b3ce6382f7f29a1d90de5d4
BoyanK95 <boyan_k_95@abv.bg>
2023-05-25 10:04:58 +0300

    working with interfaces

--- udemy-react-typescript-course/basic-interfaces/src/app.ts
+++ udemy-react-typescript-course/basic-interfaces/src/app.ts
@@ -1,18 +1,41 @@
 interface Person {
-  name: string;
-  age: number;
+    name: string;
+    age: number;
 
-  greet(phrase: string): void;
+    greet(phrase: string): void;
 }
 
 let user1: Person;
 
 user1 = {
-  name: 'Max',
-  age: 30,
-  greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
-  }
+    name: 'Max',
+    age: 30,
+    greet(phrase: string) {
+        console.log(phrase + ' ' + this.name);
+    }
 };
 
 user1.greet('Hi there - I am');
+
+interface Greetable {
+    name: string;
+
+    greet(phrase: string): void;
+}
+
+let greetBG: Greetable;
+ 
+class People implements Greetable {
+  name: string;
+  age: number;
+
+  constructor(n:string, age: number) {
+    this.name = n
+    this.age = age
+  }
+
+  greet(phrase: string): void {
+      console.log(phrase);
+      
+  }
+}
