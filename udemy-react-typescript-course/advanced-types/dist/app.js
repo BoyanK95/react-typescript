170973aa1dbb591000461f68d6f53565f78801b0
BoyanK95 <boyan_k_95@abv.bg>
2023-05-30 13:28:09 +0300

    experimenting with  combinable types

--- udemy-react-typescript-course/advanced-types/dist/app.js
+++ udemy-react-typescript-course/advanced-types/dist/app.js
@@ -2,6 +2,12 @@
 const e1 = {
     name: 'Boyan',
     privileges: ['software-dev'],
-    startDate: new Date
+    startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
 //# sourceMappingURL=app.js.map
--- udemy-react-typescript-course/advanced-types/src/app.ts
+++ udemy-react-typescript-course/advanced-types/src/app.ts
@@ -13,19 +13,38 @@ type ElevatedEmployee = Admin & Employee;
 const e1: ElevatedEmployee = {
     name: 'Boyan',
     privileges: ['software-dev'],
-    startDate: new Date
-}
-
+    startDate: new Date()
+};
 
 //With interface
 interface Admin2 {
     name: string;
-    privileges: string[]
+    privileges: string[];
 }
 
 interface Employee2 {
     name: string;
-    startDate: Date
+    startDate: Date;
 }
 
 interface ElevatedEmployee2 extends Admin2, Employee2 {}
+
+type Combinable = string | number;
+
+type Numeric = number | boolean;
+
+type Universal = Combinable | Numeric;
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') { //type guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnkownEmployee = Employee | Admin
+
+function printEmplyeeInformation(emp:UnkownEmployee) {
+    console.log('Name');
+    
+}
