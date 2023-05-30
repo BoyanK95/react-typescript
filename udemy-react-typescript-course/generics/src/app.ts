3951f90af26365695e574d87cd44273e2b1defba
BoyanK95 <boyan_k_95@abv.bg>
2023-05-30 18:36:48 +0300

    working with generic functions

--- udemy-react-typescript-course/generics/src/app.ts
+++ udemy-react-typescript-course/generics/src/app.ts
@@ -1,16 +1,27 @@
-const names : Array<string> = [] //string
-
+const names: Array<string> = []; //string
+names.push('Boyan');
+names.push('Deivid');
+names.push('Ivo');
 //Can do string operation on this arr now:
-names[0].toLowerCase()  //Have all the string methods atached 
+names[0].toLowerCase(); //Have all the string methods atached
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!!!')
-    }, 2000)
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!!!')
+//     }, 2000)
 
-    promise.then((data) => data.split(''))
-    if (!resolve) {  
-        console.log(reject);
-    }
-    
-})
+//     promise.then((data) => data.split(''))
+//     if (!resolve) {
+//         console.log(reject);
+//     }
+// })
+
+//Working with generic functions:
+function merge<T extends object, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+// console.log(merge({ firstName: 'Boyan' }, { age: 29 }));
+
+const mergedObj = merge({ firstName: 'David', hobbies: ['Eating, Sleeping'] }, { age: 23 });
+console.log(mergedObj.hobbies);
