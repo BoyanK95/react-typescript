d1ce1f36232f14170063a629b93b8aefdc7667b1
BoyanK95 <boyan_k_95@abv.bg>
2023-06-01 17:16:20 +0300

    feat(advanced-types): working with advanced types
    refresher on intersection types, type guards and discriminated unions.

--- udemy-react-typescript-course/advanced-types/dist/app.js
+++ udemy-react-typescript-course/advanced-types/dist/app.js
@@ -10,4 +10,49 @@ function add(a, b) {
     }
     return a + b;
 }
+function printEmplyeeInformation(emp) {
+    console.log('Name: ', emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start date: ' + emp.startDate);
+    }
+}
+printEmplyeeInformation(e1);
+printEmplyeeInformation({ name: 'Dinyo', privileges: ['full stack dev'] });
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving truck');
+    }
+    loadCargo(amount) {
+        console.log(`Loading cargoo...  Cargo weight is ${amount} kg.`);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('Moving at speed: ' + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 55 });
 //# sourceMappingURL=app.js.map
--- udemy-react-typescript-course/advanced-types/src/app.ts
+++ udemy-react-typescript-course/advanced-types/src/app.ts
@@ -36,15 +36,79 @@ type Numeric = number | boolean;
 type Universal = Combinable | Numeric;
 
 function add(a: Combinable, b: Combinable) {
-    if (typeof a === 'string' || typeof b === 'string') { //type guard
+    if (typeof a === 'string' || typeof b === 'string') {
+        //type guard
         return a.toString() + b.toString();
     }
     return a + b;
 }
 
-type UnkownEmployee = Employee | Admin
+type UnkownEmployee = Employee | Admin;
 
-function printEmplyeeInformation(emp:UnkownEmployee) {
-    console.log('Name ', emp);
-    
+function printEmplyeeInformation(emp: UnkownEmployee) {
+    console.log('Name: ', emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start date: ' + emp.startDate);
+    }
 }
+
+printEmplyeeInformation(e1);
+printEmplyeeInformation({ name: 'Dinyo', privileges: ['full stack dev'] });
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving truck');
+    }
+
+    loadCargo(amount: number) {
+        console.log(`Loading cargoo...  Cargo weight is ${amount} kg.`);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Hourse {
+    type: 'horse'
+    runningSpeed: number;
+}
+
+type Animal = Bird | Hourse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 55})
