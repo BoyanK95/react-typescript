114bba99b53aafc1d128480144e10045e5564133
BoyanK95 <boyan_k_95@abv.bg>
2023-06-04 22:14:27 +0300

    feat(drag-and-drop): rendered form using OOP

---
+++ udemy-react-typescript-course/drag-and-drop-project/src/app.ts
@@ -0,0 +1,21 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importendNode = document.importNode(this.templateElement.content, true);
+        this.element = importendNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+
+const projInput = new ProjectInput()
