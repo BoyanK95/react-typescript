ac41c6591a19435093c9a46b5e43086efed45597
BoyanK95 <boyan_k_95@abv.bg>
2023-06-04 22:29:53 +0300

    feat:(drag-and-drop): interacting with the DOM elements with the ProjectInput class

--- udemy-react-typescript-course/drag-and-drop-project/src/app.ts
+++ udemy-react-typescript-course/drag-and-drop-project/src/app.ts
@@ -2,14 +2,35 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         const importendNode = document.importNode(this.templateElement.content, true);
+
         this.element = importendNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement
+
+        this.configure()
         this.attach();
+    }
+
+    private submitHandler(e: Event) {
+        e.preventDefault()
+        console.log(this.titleInputElement.value);
+        
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     private attach() {
@@ -17,5 +38,4 @@ class ProjectInput {
     }
 }
 
-
-const projInput = new ProjectInput()
+const projInput = new ProjectInput();
